705bed1c3340296236286a3bd31feeb577561084
Artur Teben <teben.artur99@gmail.com>
2020-07-23 18:56:52 +0300

    Create instance of Executor class, call execute method

---
+++ src/com/java/map/Main.java
@@ -0,0 +1,11 @@
+package com.java.map;
+
+import com.java.map.executor.Executor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Executor exe = new Executor();
+        exe.execute();
+    }
+}
