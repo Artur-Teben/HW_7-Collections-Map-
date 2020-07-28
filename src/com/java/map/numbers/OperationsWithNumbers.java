21e433a01d0b28974e0c73cd773f20d519fad269
Artur Teben <teben.artur99@gmail.com>
2020-07-28 17:16:13 +0300

    Fixed bugs: change logic of method removeSimilarNumbers by using streams

--- src/com/java/map/numbers/OperationsWithNumbers.java
+++ src/com/java/map/numbers/OperationsWithNumbers.java
@@ -1,17 +1,20 @@
 package com.java.map.numbers;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class OperationsWithNumbers {
     public String dataInput() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter numbers into a line separating with a comma");
+        System.out.println("Enter numbers into a line separating with a spacebar");
         return scanner.nextLine();
     }
-    public Set<String> removeSimilarNumbers(String nums) {
-        return new HashSet<>(Arrays.asList(nums.split(",")));
+
+    public List<Integer> removeSimilarNumbers(String nums) {
+        List<String> strList = new ArrayList<>(Arrays.asList(nums.split(" ")));
+        return strList.stream()
+                .map(Integer::valueOf)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
